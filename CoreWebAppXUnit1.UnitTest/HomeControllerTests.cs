30210d49e38d34f66eacc9a0d267b14b088023b0
snerks <martin.github@outlook.com>
2018-09-24 15:13:37 +0100

    Initial commit

---
+++ CoreWebAppXUnit1.UnitTest/HomeControllerTests.cs
@@ -0,0 +1,22 @@
+using CoreWebAppXUnit1.Controllers;
+using System;
+using Xunit;
+
+namespace CoreWebAppXUnit1.UnitTest
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_succeeds()
+        {
+            // Arrange
+            var sut = new HomeController();
+
+            // Act
+            var result = sut.Index();
+
+            // Assert
+            Assert.NotNull(result);
+        }
+    }
+}
